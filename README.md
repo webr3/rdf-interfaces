8714ba5c2657b2a3471cbea9ded90eef87397f0d
webr3 <nathan@webr3.org>
2011-05-03 13:36:50 +0100

    specification implementation

---
+++ README.md
@@ -0,0 +1,21 @@
+# Implementation of the Core RDF Interfaces in ECMAScript-262 V5 (Javascript) #
+
+The [RDF Interfaces Specification](http://www.w3.org/2010/02/rdfa/sources/rdf-interfaces/) defines a set of standardized interfaces for working with RDF data in a programming environment.
+
+This is an ECMAScript-262 V5 (Javascript) implementation of those interfaces, by the specification editor.
+
+**Note:** This is a base implementation upon which libraries can be built, and which modular components can use, another library will be provided shortly which extends this implementation and adds in various DataParsers / Serializers, Query mechanisms and the usual array of utility + shorthand methods.
+
+The code uses newer features such as Object.defineProperties and Array.prototype.(some, every, forEach, filter), Data.now and Array.isArray, the implementation should be compatible with:
+ - node.js
+ - BESEN
+ - Rhino 1.7
+ - WebKit
+ - Chrome 6+
+ - Firefox 4+
+ - Safari 5+
+ - Internet Explorer 9
+ 
+ See the [ECMAScript 5 compatibility table](http://kangax.github.com/es5-compat-table/) for more up to date compatibility information.
+ 
+ This code is released in to the public domain, and [UNLICENSED](http://unlicense.org/). 
---
+++ rdfi.js
@@ -0,0 +1,385 @@
+/**
+ * ECMAScript-262 V5 Implementation of the Core RDF Interfaces
+ * 
+ *  - <http://www.w3.org/2010/02/rdfa/sources/rdf-interfaces/>
+ *  - <http://github.org/webr3/rdf-interfaces/>
+ *  
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org/>
+ */
+rdf = (function() {
+  encodeString = function(s) {
+    var out = "", skip = false, _g1 = 0, _g = s.length;
+    while(_g1 < _g) {
+      var i = _g1++;
+      if(!skip) {
+        var code = s.charCodeAt(i);
+        if(55296 <= code && code <= 56319) {
+          var low = s.charCodeAt(i + 1);
+          code = (code - 55296) * 1024 + (low - 56320) + 65536;
+          skip = true
+        }
+        if(code > 1114111) { throw new Error("Char out of range"); }
+        var hex = "00000000".concat((new Number(code)).toString(16).toUpperCase());
+        if(code >= 65536) {
+          out += "\\U" + hex.slice(-8)
+        } else {
+          if(code >= 127 || code <= 31) {
+            switch(code) {
+              case 9:  out += "\\t"; break;
+              case 10: out += "\\n"; break;
+              case 13: out += "\\r"; break;
+              default: out += "\\u" + hex.slice(-4); break
+            }
+          } else {
+            switch(code) {
+              case 34: out += '\\"'; break;
+              case 92: out += "\\\\"; break;
+              default: out += s.charAt(i); break
+            }
+          }
+        }
+      } else {
+        skip = !skip
+      }
+    }
+    return out
+  };
+  
+  BlankNode = function() {
+    return Object.defineProperties( {}, {
+      interfaceName: { writable: false, configurable : false, enumerable: true, value: 'BlankNode' },
+      value: { writable: false, configurable : false, enumerable: true, value: 'b'.concat(++BlankNode.NEXTID) },
+      valueOf: { writable: false, configurable : false, enumerable: true, value: function() {
+        return this.value;
+      }},
+      equals: { writable: true, configurable : false, enumerable: true, value: function(o) {
+        if(!o.hasOwnProperty('interfaceName')) return this.value == o;
+        return (o.interfaceName == this.interfaceName) ? this.value == o.value : false;
+      }},
+      toString: { writable: false, configurable : false, enumerable: true, value: function() {
+        return '_:'.concat(this.value);
+      }},
+      toNT: { writable: false, configurable : false, enumerable: true, value: function() {
+        return encodeString(this.toString());
+      }},
+      h: { configurable : false, enumerable: false, get: function(){return this.value} },
+    })
+  };
+  BlankNode.NEXTID = 0;
+  
+  NamedNode = function(iri) {
+    return Object.defineProperties( {}, {
+      interfaceName: { writable: false, configurable : false, enumerable: true, value: 'NamedNode' },
+      value: { writable: false, configurable : false, enumerable: true, value: iri },
+      valueOf: { writable: false, configurable : false, enumerable: true, value: function() {
+        return this.value;
+      }},
+      equals: { writable: true, configurable : false, enumerable: true, value: function(o) {
+        if(!o.hasOwnProperty('interfaceName')) return this.value == o;
+        return (o.interfaceName == this.interfaceName) ? this.value == o.value : false;
+      }},
+      toString: { writable: false, configurable : false, enumerable: true, value: function() {
+        return this.value.toString();
+      }},
+      toNT: { writable: false, configurable : false, enumerable: true, value: function() {
+        return '<' + encodeString(this.toString()) + '>';
+      }},
+      h: { configurable : false, enumerable: false, get: function(){return this.value} }
+    })
+  };
+  
+  Literal = function(value, language, datatype, nativ) {
+    if(typeof language == "string" && language[0] == "@") language = language.slice(1);
+    return Object.defineProperties( {}, {
+      interfaceName: { writable: false, configurable : false, enumerable: true, value: 'Literal' },
+      value: { writable: false, configurable : false, enumerable: true, value: value },
+      valueOf: { writable: false, configurable : false, enumerable: true, value: function() {
+        return nativ === null ? this.value : nativ;
+      }},
+      language: { writable: false, configurable : false, enumerable: true, value: language },
+      datatype: { writable: false, configurable : false, enumerable: true, value: datatype },
+      equals: { writable: true, configurable : false, enumerable: true, value: function(o) {
+        if(!o.hasOwnProperty('interfaceName')) return this.valueOf() == o;
+        if(o.interfaceName != this.interfaceName) return false;
+        return this.h == o.h;
+      }},
+      toString: { writable: false, configurable : false, enumerable: true, value: function() {
+        return this.value.toString();
+      }},
+      toNT: { writable: false, configurable : false, enumerable: true, value: function() {
+        var s = '"' + encodeString(this.value) + '"';
+        if( Boolean(this.language).valueOf() ) return s.concat('@' + this.language);
+        if( Boolean(this.datatype).valueOf() ) return s.concat('^^' + this.datatype.toNT());
+        return s;
+      }},
+      h: { writable: false, configurable : false, enumerable: false, value: language + '|' + (datatype ? datatype.toString() : '') + '|' + value.toString() }
+    })
+  };
+  
+  Triple = function(s,p,o) {
+    return Object.defineProperties( {}, {
+      subject: { writable: false, configurable : false, enumerable: true, value: s },
+      property: { writable: false, configurable : false, enumerable: true, value: p },
+      object: { writable: false, configurable : false, enumerable: true, value: o },
+      equals: { writable: true, configurable : false, enumerable: true, value: function(t) {
+        return this.s.equals(t.s) && this.p.equals(t.p) && this.o.equals(t.o);
+      }},
+      toString: { writable: false, configurable : false, enumerable: true, value: function() {
+        return this.s.toNT() + " " + this.p.toNT() + " " + this.o.toNT() + " .";
+      }},
+      s: { configurable : false, enumerable: false, get: function() { return this.subject } },
+      p: { configurable : false, enumerable: false, get: function() { return this.property } },
+      o: { configurable : false, enumerable: false, get: function() { return this.object } }
+    })
+  };
+  
+  Graph = function(a) {
+    return Object.defineProperties( {}, {
+      _graph: { writable: true, configurable : false, enumerable: false, value: [] },
+      _spo: { writable: true, configurable : false, enumerable: false, value: {} },
+      length: { configurable : false, enumerable: true, get: function() {
+        return this._graph.length;
+      }},
+      add: { writable: false, configurable : false, enumerable: true, value: function(t) {
+        this._spo[t.s.h] || (this._spo[t.s.h] = {});
+        this._spo[t.s.h][t.p.h] || (this._spo[t.s.h][t.p.h] = {});
+        if(!this._spo[t.s.h][t.p.h][t.o.h]) {
+          this._spo[t.s.h][t.p.h][t.o.h] = t;
+          this._graph.push(t);
+          this.actions.forEach(function(a){a.run(t)});
+        }
+        return this;
+      }},
+      addArray: { writable: false, configurable : false, enumerable: false, value: function(a) {
+        if(Array.isArray(a)) var g = this, b = a.forEach( function(t) { g.add(t) });
+        return this;
+      }},
+      remove: { writable: false, configurable : false, enumerable: true, value: function(t) {
+        this._spo[t.s.h] && this._spo[t.s.h][t.p.h] && this._spo[t.s.h][t.p.h][t.o.h] && (
+          delete this._spo[t.s.h][t.p.h][t.o.h] &&
+          this._graph.splice(this._graph.indexOf(t),1)  
+        );
+        return this;
+      }},
+      removeMatches: { writable: false, configurable : false, enumerable: true, value: function(s,p,o) {
+        s = arguments[0] === undefined ? null : s;
+        p = arguments[1] === undefined ? null : p;
+        o = arguments[2] === undefined ? null : o;
+        var r = [];
+        this.forEach(function(t,g) {
+          (s===null||t.s.equals(s)) && (p===null||t.p.equals(p)) && (o===null||t.o.equals(o)) && r.push(t);
+        });
+        for(i in r) this.remove(r[i]);
+        return this;
+      }},
+      toArray: { writable: false, configurable : false, enumerable: true, value: function() {
+        return this._graph.slice(0);
+      }},
+      some: { writable: false, configurable : false, enumerable: true, value: function(cb) {
+        return this._graph.some(cb);
+      }},
+      every: { writable: false, configurable : false, enumerable: true, value: function(cb) {
+        return this._graph.every(cb);
+      }},
+      filter: { writable: false, configurable : false, enumerable: true, value: function(cb) {
+        return new Graph(this._graph.filter(cb));
+      }},
+      forEach: { writable: false, configurable : false, enumerable: true, value: function(cb) {
+        var g = this; this._graph.forEach(function(t) { cb(t,g) });
+      }},
+      match: { writable: false, configurable : false, enumerable: true, value: function(s,p,o,l) {
+        s = arguments[0] === undefined ? null : s;
+        p = arguments[1] === undefined ? null : p;
+        o = arguments[2] === undefined ? null : o;
+        l = arguments[3] === undefined ? null : l;
+        var c = 0;
+        if(l<1) l=-1;
+        return new Graph(this._graph.filter(function(t) {
+          if(c == l) return false;
+          return (s===null||t.s.equals(s)) && (p===null||t.p.equals(p)) && (o===null||t.o.equals(o)) && ++c;
+        }));
+      }},
+      merge: { writable: false, configurable : false, enumerable: true, value: function(g) {
+        return new Graph()['import'](this)['import'](g);
+      }},
+      'import': { writable: false, configurable : false, enumerable: true, value: function(g) {
+        return this.addArray(g.toArray());
+      }},
+      actions: { writable: false, configurable : false, enumerable: true, value: [] },
+      addAction: { writable: false, configurable : false, enumerable: true, value: function(a,r) {
+        if(r) this.forEach(function(t,g){a.run(t,g)});
+        this.actions.push(a);
+        return this;
+      }}
+    }).addArray(a);
+  };
+  
+  TripleAction = function(test,action) {
+    return Object.defineProperties( {}, {
+      action: { writable: true, configurable : false, enumerable: true, value: action },
+      test: { writable: true, configurable : false, enumerable: true, value: test },
+      run: { writable: false, configurable : false, enumerable: true, value: function(t,g) {
+        if(this.test(t)) this.action(t,g);
+      }}    
+    })
+  };
+  
+  PrefixMap = function(i) {
+    return Object.defineProperties( {} , {
+      resolve: { writable: false, configurable : false, enumerable: false, value: function(curie) {
+        var index = curie.indexOf(":");
+        if(index < 0 || curie.indexOf("//") >= 0)  return null;
+        var prefix = curie.slice(0, index).toLowerCase();
+        if(!this[prefix]) return null;
+        return this[prefix].concat( curie.slice(++index) );
+      }},
+      shrink: { writable: false, configurable : false, enumerable: false, value: function(iri) {
+        for(pref in this)
+          if(iri.substr(0,this[pref].length) == this[pref])
+            return pref + ':' + iri.slice(this[pref].length);
+        return iri;
+      }},
+      setDefault: { writable: false, configurable : false, enumerable: false, value: function(iri) {
+        this[''] = iri;
+      }},
+      'import': { writable: false, configurable : false, enumerable: false, value: function(prefixes, override) {
+        for(p in prefixes)
+          if(!this[p] || override)
+            this[p] = prefixes[p];
+        return this;
+      }}
+    })['import'](i);
+  };
+  
+  TermMap = function(i) {
+    return Object.defineProperties( {} , {
+      resolve: { writable: false, configurable : false, enumerable: false, value: function(term) {
+        if(this[term]) return this[term]
+        if(this[""]) return this[""].concat(term)
+        return null;
+      }},
+      shrink: { writable: false, configurable : false, enumerable: false, value: function(iri) {
+        for(t in this)
+          if(this[t] == iri) return t;
+        return iri;
+      }},
+      setDefault: { writable: false, configurable : false, enumerable: false, value: function(iri) {
+        this[''] = iri;
+      }},
+      'import': { writable: false, configurable : false, enumerable: false, value: function(terms, override) {
+        for(t in terms)
+          if(!this[t] || override)
+            this[t] = terms[t];
+        return this;
+      }}
+    })['import'](i);
+  }
+  
+  Profile = function(i) {
+    return Object.defineProperties( {} , {
+      prefixes: { writable: false, configurable : false, enumerable: true, value: new PrefixMap },
+      terms: { writable: false, configurable : false, enumerable: true, value: new TermMap },
+      resolve: { writable: false, configurable : false, enumerable: false, value: function(tp) {
+        return tp.indexOf(":") >= 0 ? this.prefixes.resolve(tp) : this.terms.resolve(tp);
+      }},
+      setDefaultVocabulary: { writable: false, configurable : false, enumerable: false, value: function(iri) {
+        this.terms.setDefault(iri);
+      }},
+      setDefaultPrefix: { writable: false, configurable : false, enumerable: false, value: function(iri) {
+        this.prefixes.setDefault(iri);
+      }},
+      setTerm: { writable: false, configurable : false, enumerable: false, value: function(term, iri) {
+        this.terms[term] = iri;
+      }},
+      setPrefix: { writable: false, configurable : false, enumerable: false, value: function(prefix, iri) {
+        this.prefixes[prefix] = iri;
+      }},
+      importProfile: { writable: false, configurable : false, enumerable: false, value: function(profile, override) {
+        if(!profile) return this;
+        this.prefixes['import'](profile.prefixes, override);
+        this.terms['import'](profile.terms, override);
+        return this;
+      }}
+    }).importProfile(i);
+  };
+  
+  RDFEnvironment = function() {
+    var rp = {terms:{},prefixes:{
+      owl: "http://www.w3.org/2002/07/owl#",
+      rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+      rdfs: "http://www.w3.org/2000/01/rdf-schema#",
+      rdfa: "http://www.w3.org/ns/rdfa#",
+      xhv: "http://www.w3.org/1999/xhtml/vocab#",
+      xml: "http://www.w3.org/XML/1998/namespace",
+      xsd: "http://www.w3.org/2001/XMLSchema#",
+    }};
+    var xsd = {};
+    for(v in x=['string','boolean','dateTime','date','time','int','double','float','decimal','integer',
+              'nonPositiveInteger','negativeInteger','long','int','short','byte','nonNegativeInteger',
+              'unsignedLong','unsignedInt','unsignedShort','unsignedByte','positiveInteger'])
+      xsd[x[v]] = rp.prefixes.xsd.concat(x[v]);
+    return Object.defineProperties( new Profile(rp), {
+      createBlankNode: { writable: false, configurable : false, enumerable: false, value: function() {
+        return new BlankNode;
+      }},
+      createNamedNode: { writable: false, configurable : false, enumerable: false, value: function(iri) {
+        return new NamedNode(iri);
+      }},
+      createLiteral: { writable: false, configurable : false, enumerable: false, value: function(value) {
+        var l = null, dt = arguments[2], v = value;
+        if(arguments[1]) {
+          if(arguments[1].hasOwnProperty('interfaceName')) dt = arguments[1];
+          else l = arguments[1];
+        }
+        if(dt) {
+          switch(dt.valueOf()) {
+            case xsd.string:
+              v = new String(v); break;
+            case xsd['boolean']:
+              v = (new Boolean(v == "false" ? false : v)).valueOf(); break;
+            case xsd['float']:
+            case xsd.integer:
+            case xsd['long']:
+            case xsd['double']:
+            case xsd.decimal:
+            case xsd.nonPositiveInteger:
+            case xsd.nonNegativeInteger:
+            case xsd.negativeInteger:
+            case xsd['int']:
+            case xsd.unsignedLong:
+            case xsd.positiveInteger:
+            case xsd['short']:
+            case xsd.unsignedInt:
+            case xsd['byte']:
+            case xsd.unsignedShort:
+            case xsd.unsignedByte:
+              v = (new Number(v)).valueOf(); break;
+            case xsd['date']:
+            case xsd.time:
+            case xsd.dateTime:
+              v = new Date(v); break;
+          }
+        }
+        return new Literal(value,l,dt,v);
+      }},
+      createTriple: { writable: false, configurable : false, enumerable: false, value: function(s,p,o) {
+        return new Triple(s,p,o);
+      }},
+      createAction: { writable: false, configurable : false, enumerable: false, value: function(t,a) {
+        return new TripleAction(t,a);
+      }},
+      createProfile: { writable: false, configurable : false, enumerable: false, value: function(empty) {
+        return new Profile(!empty ? this : null);
+      }},
+      createTermMap: { writable: false, configurable : false, enumerable: false, value: function(empty) {
+        return new TermMap(!empty ? this.terms : null);
+      }},
+      createPrefixMap: { writable: false, configurable : false, enumerable: false, value: function(empty) {
+        return new PrefixMap(!empty ? this.prefixes : null);
+      }},
+    });
+  };
+  return new RDFEnvironment;
+})();
+if(module) module.exports = rdf; 
